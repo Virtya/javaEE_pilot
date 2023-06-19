627040a975e749ea07b4d0b766d03e9656d0aac3
virtya <nik.zheltikov.26@gmail.com>
2023-06-19 17:16:51 +0300

    first com

---
+++ src/main/java/ru/ds/javaEE_pilot/JavaEePilotApplication.java
@@ -0,0 +1,13 @@
+package ru.ds.javaEE_pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEePilotApplication.class, args);
+	}
+
+}
