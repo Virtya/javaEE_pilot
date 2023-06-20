e94a1c367a4624ba53c73f96aae478ed78fb96b5
virtya <nik.zheltikov.26@gmail.com>
2023-06-20 19:07:24 +0300

    first com

--- src/main/java/ru/ds/education/currency/dto/CursDataDto.java
+++ src/main/java/ru/ds/education/currency/dto/CursDataDto.java
@@ -2,8 +2,6 @@ package ru.ds.education.currency.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.codehaus.commons.nullanalysis.NotNull;
-import org.codehaus.commons.nullanalysis.Nullable;
 
 import java.time.LocalDate;
 
